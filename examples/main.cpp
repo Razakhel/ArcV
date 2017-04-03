07b522faa1f532c763ac57e039fd8e090cf61efa
Razakhel <romain.milbert@gmail.com>
2017-04-03 12:56:04 +0200

    [Update] Implemented basic vector functions

--- examples/main.cpp
+++ examples/main.cpp
@@ -1,3 +1,5 @@
+#include <iostream>
+
 #include "ArcV.hpp"
 
 int main(int argc, char* argv[]) {
@@ -7,10 +9,16 @@ int main(int argc, char* argv[]) {
 
   Arcv::Mat mat(3000, 3000);
 
-  /*Arcv::Window window;
-  window.create(1000, 1000);
-  window.mapImage(img);
-  window.show();*/
+  Arcv::Vec vec(3);
+  for (unsigned int i = 0; i < vec.getData().size(); ++i)
+    vec[i] = i * 2;
+  vec *= 3;
+
+  Arcv::Vec vec2(3);
+  for (unsigned int i = 0; i < vec2.getData().size(); ++i)
+    vec2[i] = i * 3;
+
+  std::cout << vec.dot(vec2) << std::endl; // 90
 
   return 0;
 }
--- src/ArcV/Math/Vec.cpp
+++ src/ArcV/Math/Vec.cpp
@@ -2,4 +2,47 @@
 
 namespace Arcv {
 
+const float Vec::dot(Vec& vec) const {
+  float res = 0.f;
+  for (unsigned int i = 0; i < data.size(); ++i)
+    res += data[i] * vec.getData()[i];
+  return res;
+}
+
+Vec& Vec::operator+(Vec& vec) {
+  for (unsigned int i = 0; i < data.size(); ++i)
+    data[i] += vec.getData()[i];
+  return *this;
+}
+
+Vec& Vec::operator+(const float& val) {
+  for (unsigned int i = 0; i < data.size(); ++i)
+    data[i] += val;
+  return *this;
+}
+
+Vec& Vec::operator-(Vec& vec) {
+  for (unsigned int i = 0; i < data.size(); ++i)
+    data[i] -= vec.getData()[i];
+  return *this;
+}
+
+Vec& Vec::operator-(const float& val) {
+  for (unsigned int i = 0; i < data.size(); ++i)
+    data[i] -= val;
+  return *this;
+}
+
+Vec& Vec::operator*(Vec& vec) {
+  for (unsigned int i = 0; i < data.size(); ++i)
+    data[i] *= vec.getData()[i];
+  return *this;
+}
+
+Vec& Vec::operator*(const float& val) {
+  for (unsigned int i = 0; i < data.size(); ++i)
+    data[i] *= val;
+  return *this;
+}
+
 } // namespace Arcv
