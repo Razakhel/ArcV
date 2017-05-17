18c2adf3cdf6dc8859f5a6ced3b2207429c6eb27
Razakhel <romain.milbert@gmail.com>
2017-05-17 14:23:57 +0200

    [Fix] Convolution presumably correct again

--- src/ArcV/Math/Matrix.cpp
+++ src/ArcV/Math/Matrix.cpp
@@ -6,21 +6,19 @@
 namespace Arcv {
 
 template <>
-void Matrix<uint8_t>::convolve(const Matrix<float>& convMat) {
+void Matrix<>::convolve(const Matrix<float>& convMat) {
   assert(("Error: Convolution matrix must be a square one", convMat.getWidth() == convMat.getHeight()));
   assert(("Error: Convolution matrix's size must be odd", convMat.getData().size() % 2 == 1));
 
   // TODO: make this work for any number of channels
   const Mat tempMat = *this;
 
-  const uint8_t channels = Image::getChannelCount(tempMat);
-
   for (unsigned int matHeightIndex = 0; matHeightIndex < tempMat.getHeight(); ++matHeightIndex) {
     for (unsigned int matWidthIndex = 0; matWidthIndex < tempMat.getWidth() * channels; matWidthIndex += channels) {
       const unsigned int matIndex = (matHeightIndex * tempMat.getWidth()) * channels + matWidthIndex;
 
-      for (int convHeightIndex = -(convMat.getData().size() - 1) / 2; convHeightIndex < (convMat.getData().size() - 1) / 2; ++convHeightIndex) {
-        for (int convWidthIndex = -(convMat.getData().size() - 1) / 2; convWidthIndex < (convMat.getData().size() - 1) / 2; ++convWidthIndex) {
+      for (int convHeightIndex = -((convMat.getHeight() - 1) / 2); convHeightIndex < (convMat.getHeight() - 1) / 2; ++convHeightIndex) {
+        for (int convWidthIndex = -((convMat.getWidth() - 1) / 2); convWidthIndex < (convMat.getWidth() - 1) / 2; ++convWidthIndex) {
           const unsigned int correspHeight = matHeightIndex + convHeightIndex;
           const unsigned int correspWidth = matWidthIndex + convWidthIndex;
 
