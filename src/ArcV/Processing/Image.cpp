4b1f89ef2c18b8c010d97e4a888cfc7f77bdf5a9
Romain Milbert <romain.milbert@gmail.com>
2017-07-25 17:37:59 +0200

    [Update] Huge refactor, replaced ugly switches by template spec in Image

--- src/ArcV/Processing/Image.cpp
+++ src/ArcV/Processing/Image.cpp
@@ -13,7 +13,6 @@ namespace {
 
 bool validatePng(std::istream& file) {
   std::array<png_byte, PNG_HEADER_SIZE> header;
-
   file.read(reinterpret_cast<char*>(header.data()), PNG_HEADER_SIZE);
 
   return (png_sig_cmp(header.data(), 0, PNG_HEADER_SIZE) == 0);
@@ -27,6 +26,48 @@ void readPng(png_structp pngReadPtr, png_bytep data, png_size_t length) {
 void writePng(png_structp pngWritePtr, png_bytep data, png_size_t length) {
   png_voidp outPtr = png_get_io_ptr(pngWritePtr);
   static_cast<std::ostream*>(outPtr)->write(reinterpret_cast<const char*>(data), length);
+}
+
+void addAlphaChannel(Matrix<>& mat) {
+  const Matrix<float> tempMat = mat;
+
+  mat.setChannelCount(static_cast<uint8_t>(mat.getChannelCount() + 1));
+  mat.getData().resize(mat.getWidth() * mat.getHeight() * mat.getChannelCount());
+
+  for (std::size_t i = 0, delta = 0; i < tempMat.getData().size(); i += tempMat.getChannelCount(), ++delta) {
+    for (std::size_t chan = i; chan < i + tempMat.getChannelCount(); ++chan)
+      mat.getData()[chan + delta] = tempMat.getData()[chan];
+
+    // Filling the alpha value with full opacity by default
+    mat.getData()[i + tempMat.getChannelCount() + delta] = 255;
+  }
+}
+
+void removeAlphaChannel(Matrix<>& mat) {
+  // No need to start at the very beginning since the first channel pack will not be moved
+  for (std::size_t i = mat.getChannelCount(), delta = 1; i < mat.getData().size(); i += mat.getChannelCount(), ++delta) {
+    for (std::size_t chan = i; chan < i + mat.getChannelCount() - 1; ++chan)
+      mat.getData()[chan - delta] = mat.getData()[chan];
+  }
+
+  mat.setChannelCount(static_cast<uint8_t>(mat.getChannelCount() - 1));
+  mat.getData().resize(mat.getWidth() * mat.getHeight() * mat.getChannelCount());
+}
+
+Matrix<> computeHorizontalSobelOperator(Matrix<>& mat) {
+  const Matrix<float> horizKernel = {{  1.f,  2.f,  1.f },
+                                     {  0.f,  0.f,  0.f },
+                                     { -1.f, -2.f, -1.f }};
+
+  return mat.convolve(horizKernel);
+}
+
+Matrix<> computeVerticalSobelOperator(Matrix<>& mat) {
+  const Matrix<float> vertKernel = {{ 1.f, 0.f, -1.f },
+                                    { 2.f, 0.f, -2.f },
+                                    { 1.f, 0.f, -1.f }};
+
+  return mat.convolve(vertKernel);
 }
 
 } // namespace
@@ -104,7 +145,7 @@ Matrix<> Image::read(const std::string& fileName) {
 
   png_read_image(pngReadStruct, rowPtrs.data());
   png_read_end(pngReadStruct, pngInfoStruct);
-  png_destroy_read_struct(&pngReadStruct, 0, &pngInfoStruct);
+  png_destroy_read_struct(&pngReadStruct, nullptr, &pngInfoStruct);
 
   return Matrix<>(mat);
 }
@@ -167,6 +208,181 @@ void Image::write(const Matrix<>& mat, const std::string& fileName) {
 
   png_write_end(pngWriteStruct, pngInfoStruct);
   png_destroy_write_struct(&pngWriteStruct, &pngInfoStruct);
+}
+
+template <>
+Matrix<> Image::changeColorspace<ARCV_COLORSPACE_GRAY>(Matrix<> mat) {
+  std::size_t index = 0;
+  // Avoiding alpha channel, not including it into the operation
+  const uint8_t alpha = static_cast<uint8_t>(mat.getColorspace() >= ARCV_COLORSPACE_GRAY_ALPHA ? 1 : 0);
+  const uint8_t stride = mat.getChannelCount() - alpha;
+
+  for (auto elt = mat.getData().begin(); elt != mat.getData().end(); elt += mat.getChannelCount(), index += 1 + alpha) {
+    mat.getData()[index] = std::accumulate(elt, elt + stride, 0.f) / stride;
+
+    if (alpha)
+      mat.getData()[index + 1] = *(elt + stride);
+  }
+
+  mat.setChannelCount(static_cast<uint8_t>(1 + alpha));
+  mat.getData().resize(mat.getWidth() * mat.getHeight() * mat.getChannelCount());
+
+  if (mat.getColorspace() >= ARCV_COLORSPACE_GRAY_ALPHA)
+    removeAlphaChannel(mat);
+  mat.setColorspace(ARCV_COLORSPACE_GRAY);
+
+  return mat;
+}
+
+template <>
+Matrix<> Image::changeColorspace<ARCV_COLORSPACE_RGB>(Matrix<> mat) {
+  assert(("Warning: Function not handled yet", mat.getColorspace() == ARCV_COLORSPACE_RGBA));
+
+  if (mat.getColorspace() >= ARCV_COLORSPACE_GRAY_ALPHA)
+    removeAlphaChannel(mat);
+  mat.setColorspace(ARCV_COLORSPACE_RGB);
+
+  return mat;
+}
+
+template <>
+Matrix<> Image::changeColorspace<ARCV_COLORSPACE_HSV>(Matrix<> mat) {
+  assert(("Error: Input matrix's colorspace should be RGB(A)",
+      mat.getColorspace() == ARCV_COLORSPACE_RGB || mat.getColorspace() == ARCV_COLORSPACE_RGBA));
+
+  for (auto elt = mat.getData().begin(); elt != mat.getData().end(); elt += mat.getChannelCount()) {
+    const float red = *elt / 255;
+    const float green = *(elt + 1) / 255;
+    const float blue = *(elt + 2) / 255;
+
+    const float minVal = std::min(std::min(red, green), blue);
+    const float maxVal = std::max(std::max(red, green), blue);
+    const float delta = maxVal - minVal;
+
+    // Hue
+    float hue = 0.f;
+
+    if (delta == 0) {
+      *elt = 0;
+    } else {
+      if (maxVal == red) {
+        hue = 60 * (green - blue) / delta;
+
+        if (hue < 0.f)
+          hue += 360;
+      } else if (maxVal == green) {
+        hue = 120 + 60 * (blue - red) / delta;
+      } else if (maxVal == blue) {
+        hue = 240 + 60 * (red - green) / delta;
+      }
+
+      *elt = hue / 2;
+    }
+
+    // Saturation
+    *(elt + 1) = (maxVal == 0 ? 0 : delta / maxVal * 255);
+
+    // Value
+    *(elt + 2) = maxVal * 255;
+  }
+
+  if (mat.getColorspace() >= ARCV_COLORSPACE_GRAY_ALPHA)
+    removeAlphaChannel(mat);
+  mat.setColorspace(ARCV_COLORSPACE_HSV);
+
+  return mat;
+}
+
+template <>
+Matrix<> Image::changeColorspace<ARCV_COLORSPACE_GRAY_ALPHA>(Matrix<> mat) {
+  mat = changeColorspace<ARCV_COLORSPACE_GRAY>(mat);
+
+  if (mat.getColorspace() < ARCV_COLORSPACE_GRAY_ALPHA)
+    addAlphaChannel(mat);
+  mat.setColorspace(ARCV_COLORSPACE_GRAY_ALPHA);
+
+  return mat;
+}
+
+template <>
+Matrix<> Image::changeColorspace<ARCV_COLORSPACE_RGBA>(Matrix<> mat) {
+  assert(("Warning: Function not handled yet", mat.getColorspace() == ARCV_COLORSPACE_RGB));
+
+  if (mat.getColorspace() < ARCV_COLORSPACE_GRAY_ALPHA)
+    addAlphaChannel(mat);
+  mat.setColorspace(ARCV_COLORSPACE_RGBA);
+
+  return mat;
+}
+
+template <>
+Matrix<> Image::applyFilter<ARCV_FILTER_TYPE_GAUSSIAN_BLUR>(Matrix<> mat) {
+  Matrix<float> kernel = {{ 1.f,  4.f,  6.f,  4.f, 1.f },
+                          { 4.f, 16.f, 24.f, 16.f, 4.f },
+                          { 6.f, 24.f, 36.f, 24.f, 6.f },
+                          { 4.f, 16.f, 24.f, 16.f, 4.f },
+                          { 1.f,  4.f,  6.f,  4.f, 1.f }};
+
+  return mat.convolve(kernel / 256);
+}
+
+template <>
+Matrix<> Image::applyFilter<ARCV_FILTER_TYPE_SHARPEN>(Matrix<> mat) {
+  const Matrix<float> kernel = {{ 0.f,  -1.f,  0.f },
+                                { -1.f,  5.f, -1.f },
+                                { 0.f,  -1.f,  0.f }};
+
+  return mat.convolve(kernel);
+}
+
+template <>
+Matrix<> Image::applyFilter<ARCV_FILTER_TYPE_EDGE_DETECTION>(Matrix<> mat) {
+  const Matrix<float> kernel = {{ 0.f,   1.f,  0.f },
+                                { 1.f,  -4.f,  1.f },
+                                { 0.f,   1.f,  0.f }};
+
+  return mat.convolve(kernel);
+}
+
+template <>
+Matrix<> Image::applyFilter<ARCV_FILTER_TYPE_EMBOSS>(Matrix<> mat) {
+  const Matrix<float> kernel = {{ -2.f, -1.f, 0.f },
+                                { -1.f,  1.f, 1.f },
+                                {  0.f,  1.f, 2.f }};
+
+  return mat.convolve(kernel);
+}
+
+template <>
+Matrix<> Image::applyFilter<ARCV_FILTER_TYPE_SOBEL>(Matrix<> mat) {
+  const Matrix<float> horizRes = computeHorizontalSobelOperator(mat);
+  const Matrix<float> vertRes = computeVerticalSobelOperator(mat);
+
+  for (std::size_t i = 0; i < mat.getData().size(); ++i)
+    mat.getData()[i] = std::sqrt(vertRes.getData()[i] * vertRes.getData()[i]
+                                     + horizRes.getData()[i] * horizRes.getData()[i]);
+
+  return mat;
+}
+
+template <>
+Matrix<> Image::applyDetector<ARCV_DETECTOR_TYPE_HARRIS>(Matrix<> mat) {
+  mat = Image::changeColorspace<ARCV_COLORSPACE_GRAY>(mat);
+
+  Matrix<float> horizRes = computeHorizontalSobelOperator(mat);
+  Matrix<float> vertRes = computeVerticalSobelOperator(mat);
+
+  Matrix<float> res = horizRes * vertRes;
+  horizRes *= horizRes;
+  vertRes *= vertRes;
+
+  for (std::size_t i = 0; i < mat.getData().size(); ++i) {
+    if ((horizRes.getData()[i] * vertRes.getData()[i] - res.getData()[i] * res.getData()[i])
+        - 0.04f * std::pow(horizRes.getData()[i] + vertRes.getData()[i], 2.f) > 255)
+      mat.getData()[i] = 255;
+  }
+
+  return mat;
 }
 
 } // namespace Arcv
